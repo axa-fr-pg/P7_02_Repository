ec6423240371527c8ee0fa835e2ef08f247bdd3b
philippegeyaxa <philippe.gey@axa.fr>
2020-05-28 16:19:01 +0200

    added lombok

--- src/main/java/swa/poseidon/domain/BidList.java
+++ src/main/java/swa/poseidon/domain/BidList.java
@@ -2,14 +2,28 @@ package swa.poseidon.domain;
 
 import org.springframework.beans.factory.annotation.Required;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
 import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
 import java.sql.Date;
 import java.sql.Timestamp;
 
 @Entity
 @Table(name = "bidlist")
+@FieldDefaults(level=AccessLevel.PRIVATE)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BidList {
     // TODO: Map columns in data table BIDLIST with corresponding java fields
 	@Id
